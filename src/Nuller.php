711c77810a3517d5905410608c8258d6962da1f9
Val Luminarias <vluminarias@yahoo.com>
2016-03-28 15:33:51 +0800

    Add default string placeholder and Add some Doc Blocks

--- src/Nuller.php
+++ src/Nuller.php
@@ -3,8 +3,16 @@ namespace Nuller;
 
 Class Nuller {
 
+
     /**
-     * @param $string String to be nulled
+     * @var string The default string placeholder
+     */
+    public static $default_string_placeholder = 'Null';
+
+    /**
+     * Nullified String
+     *
+     * @param $string String to be nullified
      * @param bool|false $as_string returns desired string value if true
      * @param string $display_string String to be returned if as_string is true
      * @return null|string
@@ -13,10 +21,24 @@ Class Nuller {
         return $as_string ? self::nullifiedToString($string, $display_string) : null;
     }
 
+    /**
+     * Nullified to Desired String Placeholder
+     *
+     * @param $string String to be nullified
+     * @param string $display_string String to replace as placeholder
+     * @return string
+     */
     public static function nullifiedToString($string, $display_string = '') {
-        return $display_string ?: 'Null';
+        return $display_string ?: self::$default_string_placeholder;
     }
 
+    /**
+     * Nullified String only if Empty
+     *
+     * @param $string String to be nullified
+     * @param string $display_string String to replace as placeholder
+     * @return string
+     */
     public static function nullifiedIfEmpty($string, $display_string = '') {
         return $string ?: self::nullifiedToString($string, $display_string);
     }
